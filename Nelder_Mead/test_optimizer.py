6d2744dca39410fd7e537bb15c4e204a8a0e9c8f
Sumith Yesudasan <ydsumith@users.noreply.github.com>
2020-03-10 11:52:56 -0500

    Update test_optimizer.py

--- Nelder_Mead/test_optimizer.py
+++ Nelder_Mead/test_optimizer.py
@@ -16,21 +16,30 @@ def create_initial_simplex(x,n,dh):
     for i in range(n):
         x_mat[0][i] = x[i];
     for j in range(1,n+1): # row navigation
-        for i in range(n):
+        for i in range(n): # column nav
             if i == j-1:
                 x_mat[j][i] = x[i] + dh;
             else:
                 x_mat[j][i] = x[i];
-    ret_val = x_mat;
-    return ret_val
+    return x_mat
+
+def sort_mats(cost_mat,x_mat,n):
+
+    return cost_mat,x_mat;
 
 def main():
     n = 5; dh = 0.01;
     x = [0.1, 1.2, 5.0, 3.0, -1.0];
-    ret_val = create_initial_simplex(x,n,dh);
-    print(ret_val);
-    cost = cost_estimate(x,n);
-    print("Cost value = ", cost);
+    cost_mat = np.zeros((n+1,1));
+    x_mat = create_initial_simplex(x,n,dh);
+    print("Here is the initial simplex\n",x_mat);
+    for i in range(n+1): # from x_i to x_n+1
+        cost_mat[i] = cost_estimate(x_mat[i][:],n);
+    print("\nCost value = \n", cost_mat);
+    # sorting
+    (cost_mat,x_mat) = sort_mats(cost_mat,x_mat,n);
+    print("\nSorted x values\n",x_mat);
+    print("\nsorted cost value = \n", cost_mat);
 
 if __name__ == "__main__":
     main()
